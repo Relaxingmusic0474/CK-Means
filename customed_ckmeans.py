d18b6cdada20d6ba6673b65fad28d314a830e80f
Relaxingmusic0474 <andywalls.s17@gmail.com>
2025-12-27 03:20:54 -0400

    Se agregó aún más marco teórico

---
+++ customed_ckmeans.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def ckmeans_1d(data, k):
+    """
+    Docstring for ckmeans_1d
+    
+    :param data: El conjunto de datos a particionar.
+    :param k: El número de clusters
+    """
+    
